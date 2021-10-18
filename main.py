ebf47c490dd17d6aee7c8bd5a6d14318964a3143
Imaad Fakier <79690509+imaadfakier@users.noreply.github.com>
2021-10-18 21:16:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-# import random
-# import random as rando
 import random as random
 
 app = Flask(__name__)
